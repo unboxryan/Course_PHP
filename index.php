cee9a82103c227c17f2ab2e558601794dc84e2a6
Ryan Siah <ryan.unbox@gmail.com>
2020-12-26 15:03:23 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+    <?php include('variables.php'); ?>
+
+<!doctype html>
+
+<title> Un-Code by Ryan </title>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<body>
+    <?php include('NavBar.php'); ?>
+<center>
+<br/>
+
+<div class="container"> 
+<div class="jumbotron">
+    <h1 class="display-4"> Welcome to Un-Code by Ryan</h1>
+    <p class="lead"> This is only the beginning of Ryan's coding journey!</p>
+    <hr class="my-4">
+    <p> This syntax or format code copied from bootstrap.</p>
+</div>
+</div>
+
+    <?php
+    include("footer.php");
+    ?>
+
+</center>
+</body>
+</head>
+
---
+++ variables.php
@@ -0,0 +1,7 @@
+<?php
+
+    $company_name = "Un-Code";
+    $tagline = "Code-freak since 2020";
+
+
+?>
